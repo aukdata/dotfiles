a9a2f0c6a95cf09a415cf967e0a85511e9634d8e
takuma <takuma.y0524@gmail.com>
2021-11-01 02:18:39 +0900

    fix

--- bin/git.sh
+++ bin/git.sh
@@ -2,9 +2,6 @@
 
 echo 'Configrating git...'
 
-git config --global user.name takuma
-git config --global user.email takuma.y0524@gmail.com
-
 mkdir -p ~/.config/git
 cp -f "./home/.config/git/$OS.gitignore" "$HOME/.config/git/ignore"
 
